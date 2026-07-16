ddeaa822bb511660f07070c92c9dbca1d9b4cf2a
Abba Kassallah Kerrey <kerreyabbakassallah@gmail.com>
2026-07-16 23:24:04 +0300

    premier commit

---
+++ Day37/main.py
@@ -0,0 +1,38 @@
+import requests
+
+PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+USERNAME = "kerrey"
+TOKEN = "guvecdlbncjkvnifv"
+
+# Create user (only run once)
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+print(response.text)
+
+# Create graph
+graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(
+    url=graph_endpoint,
+    json=graph_config,
+    headers=headers
+)
+print(response.text)
